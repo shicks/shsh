81b58f787cf277617519a566fdddd50d0bc46706
David Roundy <droundy@darcs.net>
2008-10-16 13:10:42 -0700

    simplify haskeline check.
    Ignore-this: bd6ef29a858535a88af2a1294ee1b828
    
    darcs-hash:20081016201042-72aca-1f2e978a357e7e7dc11567210fd00e9f4372d33b

--- Setup.hs
+++ Setup.hs
@@ -2,18 +2,9 @@
 import Distribution.Franchise
 
 configure = do ghcFlags ["-threaded","-O2"]
-               checkHaskeline
-               buildable
-               return ()
+               requireModule "System.Console.Haskeline"
+                 `catchC` \_ -> putS "Packages haskeline not found."
 
 buildable = executable "shsh" "shsh.lhs" []
 
 main = build [] configure buildable
-
-checkHaskeline =
-    unlessC (haveExtraData "NO_HASKELINE") $
-    do requireModuleExporting "System.Console.Haskeline"
-           "runInputT, defaultSettings, getInputLine, handleDyn"
-           "runInputT defaultSettings (getInputLine \"prompt: \") :: IO (Maybe String)"
-       define "HAVE_HASKELINE"
-     `catchC` \_ -> putS "Haskeline not found."
