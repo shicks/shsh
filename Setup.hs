30c13b683347718db1e92da2e04bbf3b68e25eba
Stephen Hicks <sdh33@cornell.edu>
2008-10-19 19:53:53 -0700

    Use withModule in setup
    Ignore-this: cf19ebacb158a03aa9386f8ee8f2694
    
    darcs-hash:20081020025353-0087a-be13e3f3a5e6a0f9b73c01b435b00685b2ed784a

--- Setup.hs
+++ Setup.hs
@@ -2,8 +2,7 @@
 import Distribution.Franchise
 
 configure = do ghcFlags ["-threaded","-O2"]
-               requireModule "System.Console.Haskeline"
-                 `catchC` \_ -> putS "Packages haskeline not found."
+               withModule "System.Console.Haskeline" $ define "HAVE_HASKELINE"
 
 buildable = executable "shsh" "shsh.lhs" []
 
