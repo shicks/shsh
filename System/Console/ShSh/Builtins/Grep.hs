a37ed93169abe93346b62cabb6ca75e65a0ce75d
David Roundy <droundy@darcs.net>
2008-12-15 17:22:10 -0800

    extend grep support a bit.
    Ignore-this: d454859bcb987ef706c2217fc8331f0aded74857
    
    darcs-hash:20081216012210-72aca-f579a3ac9b91b3cc34f42ee07aafc3c46c55fda8

--- System/Console/ShSh/Builtins/Grep.hs
+++ System/Console/ShSh/Builtins/Grep.hs
@@ -9,37 +9,51 @@ import Control.Monad.Trans ( liftIO )
 import Data.Bits ( (.|.) )
 import System.Console.GetOpt
 import System.Exit ( ExitCode(..) )
-import Text.Regex.Posix ( match, Regex,
-                          makeRegexOpts, compNewline, defaultExecOpt,
-                          compIgnoreCase )
+import Text.Regex.Posix ( Regex, compNewline, compIgnoreCase )
+import Text.Regex.Base ( match, makeRegexOpts, defaultExecOpt )
 
 grep :: [String] -> Shell ExitCode
-grep = withArgs "grep" header args RequireOrder $ \ws -> do
-          ami <- flag 'i'
-          amv <- flag 'v'
-          let grepit reg = xor amv . match (makeRegexOpts compOpt
-                                            defaultExecOpt reg :: Regex)
-              compOpt = if ami then compNewline .|. compIgnoreCase
-                               else compNewline
-              xor a = if a then not else id
-          case ws of
-            [] -> fail "grep requires an argument!"
-            [p] -> do x <- iGetContents
-                      case filter (grepit p) $ lines x of
-                        [] -> return $ ExitFailure 1
-                        ls -> do oPutStr $ unlines ls
-                                 return ExitSuccess
-            (p:fs) -> do x <- mapM (liftIO . readFile) fs
-                         let fm = zip fs $ map (filter (grepit p) . lines) x
-                             pretty (f,ls) = if length fs > 1
-                                             then map ((f++":")++) ls
-                                             else ls
-                         if null $ concatMap snd fm
-                           then return $ ExitFailure 1
-                           else do oPutStr $ unlines $ concatMap pretty fm
-                                   return ExitSuccess
-    where header = "Usage: grep [-iv] args...\n"++
+grep = withArgs "grep" header args RequireOrder grep'
+    where grep' [] = fail "grep requires an argument!"
+          grep' (reg:origfs) =
+              do ami <- flag 'i'
+                 amv <- flag 'v'
+                 amw <- flag 'w'
+                 amn <- flag 'n'
+                 amH <- (|| length origfs > 1) `fmap` flag 'H'
+                 let fs = if null origfs then ["-"] else origfs
+                     matchIt r = match (makeRegexOpts compOpt
+                                        defaultExecOpt r :: Regex)
+                     nonword = "[^0-9a-zA-Z]"
+                     grepIt l = xor amv $
+                                if not amw
+                                then matchIt reg l
+                                else or $ map (matchIt `flip` l)
+                                          [b++reg++e | b <- ["^",nonword]
+                                                     , e <- ["$",nonword]]
+                     compOpt = if ami then compNewline .|. compIgnoreCase
+                                      else compNewline
+                     xor a = if a then not else id
+                     readStuff = do xs <- mapM readf fs
+                                    return $ concatMap cleanup xs
+                         where cleanup (f,x) = map cleanup' $ zip [1::Int ..] $ lines x
+                                   where cleanup' (n,l) =
+                                             if amn then (prefix++show n++":",l)
+                                                    else (prefix,l)
+                                         prefix = if amH then (f++":") else ""
+                               readf "-" = (,) "(standard input)" `fmap` iGetContents
+                               readf f = (,) f `fmap` liftIO (readFile f)
+                 x <- filter (grepIt . snd) `fmap` readStuff
+                 if null x
+                     then return $ ExitFailure 1
+                     else do oPutStr $ unlines $ map (uncurry (++)) x
+                             return ExitSuccess
+          header = "Usage: grep [-iv] args...\n"++
                    "grep it, silly."
           args = [flagOn "i" [] 'i' "case insensitive grep",
                   flagOn "v" [] 'v' "invert match",
-                  flagOn "HenwF" [] 'z' "flags we don't support"]
+                  flagOn "w" [] 'w' "match on words",
+                  flagOn "n" [] 'n' "line numbers",
+                  flagOn "H" [] 'H' "show file names",
+                  flagOn "eF" [] 'z' "flags we don't support"]
+
