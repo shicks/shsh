ab998eb6fd82f3185df44e6ce494d3b7feefa042
David Roundy <droundy@darcs.net>
2008-12-17 13:49:01 -0800

    implement simple touch builtin.
    Ignore-this: 687c9c1389c913b121f7ae37525096f4b78e3132
    
    darcs-hash:20081217214901-72aca-5505a0cd43c97c6452d98f10ceb4c2687b04e373

---
+++ System/Console/ShSh/Builtins/Touch.hs
@@ -0,0 +1,16 @@
+{-# OPTIONS_GHC -Wall #-}
+module System.Console.ShSh.Builtins.Touch ( touch ) where
+
+import System.Console.ShSh.Shell ( Shell )
+
+import System.IO ( openFile, IOMode(..), hClose )
+import Control.Monad.Trans ( liftIO )
+import System.Exit ( ExitCode(..) )
+
+touch :: [String] -> Shell ExitCode
+touch [] = fail "missing file operand"
+touch fs = do mapM_ touch'' fs
+              return ExitSuccess
+    where touch'' x = liftIO (openFile x WriteMode >>= hClose)
+
+
