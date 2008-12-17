b64c0f9fc4a89d1322be9e1d4032ece5689b5cb5
David Roundy <droundy@darcs.net>
2008-12-17 14:20:00 -0800

    add simple date command.
    Ignore-this: dedb773177cda7bc07a1ca202e9f3b531675d5c1
    
    darcs-hash:20081217222000-72aca-7816b7ecdfeba2f303201166a39dac8718f902be

---
+++ System/Console/ShSh/Builtins/Date.hs
@@ -0,0 +1,17 @@
+{-# OPTIONS_GHC -Wall #-}
+module System.Console.ShSh.Builtins.Date ( date ) where
+
+import System.Console.ShSh.IO ( oPutStrLn )
+import System.Console.ShSh.Shell ( Shell )
+
+import System.Time ( getClockTime, toCalendarTime, calendarTimeToString )
+import Control.Monad.Trans ( liftIO )
+import System.Exit ( ExitCode(..) )
+
+date :: [String] -> Shell ExitCode
+date _ = do clockt <- liftIO getClockTime
+            ct <- liftIO $ toCalendarTime clockt
+            oPutStrLn $ calendarTimeToString ct
+            return ExitSuccess
+
+
