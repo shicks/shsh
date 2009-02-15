cdf0cd88cd13cbe63a85956e73c5ebcd991b6300
David Roundy <droundy@darcs.net>
2009-02-15 09:42:50 -0800

    fix touch to actually change the file (and back again).
    Ignore-this: eeed08d9d83e80c242f5d02268aef385fd78dd0c
    
    darcs-hash:20090215174250-72aca-d0c324778d8215b2726c6c49fd93b7810e66231b

--- System/Console/ShSh/Builtins/Touch.hs
+++ System/Console/ShSh/Builtins/Touch.hs
@@ -3,7 +3,7 @@ module System.Console.ShSh.Builtins.Touch ( touch ) where
 
 import System.Console.ShSh.Shell ( Shell )
 
-import System.IO ( openFile, IOMode(..), hClose )
+import System.IO ( openFile, IOMode(..), hClose, hFileSize, hSetFileSize )
 import Control.Monad.Trans ( liftIO )
 import System.Exit ( ExitCode(..) )
 
@@ -12,6 +12,12 @@ touch :: [String] -> Shell ExitCode
 touch [] = fail "missing file operand"
 touch fs = do mapM_ touch'' fs
               return ExitSuccess
-    where touch'' x = liftIO (openFile x AppendMode >>= hClose)
+    where touch'' x = liftIO $ do h <- openFile x AppendMode
+                                  s <- hFileSize h
+                                  hSetFileSize h (s+1)
+                                  hClose h
+                                  h2 <- openFile x AppendMode
+                                  hSetFileSize h2 s
+                                  hClose h2
 
 
