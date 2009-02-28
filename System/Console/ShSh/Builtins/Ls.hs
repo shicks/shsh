a3e99ee91126e2ec4ed3bd857e27d3292e43bea7
David Roundy <droundy@darcs.net>
2009-02-28 11:45:29 -0800

    expand ls -l just a bit.
    Ignore-this: 3f0f8216892c7bfa5c9531468643872b79caf0e3
    It still doesn't include all the information it ought, but it includes
    a bit of it.
    
    darcs-hash:20090228194529-72aca-a6b6692f677fb477e344fb5191fa407c1f9f8a66

--- System/Console/ShSh/Builtins/Ls.hs
+++ System/Console/ShSh/Builtins/Ls.hs
@@ -1,34 +1,48 @@
-{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wall -cpp #-}
 module System.Console.ShSh.Builtins.Ls ( ls ) where
 
 import System.Console.ShSh.IO ( oPutStr, oPutStrLn )
 import System.Console.ShSh.Shell ( Shell )
-import System.Console.ShSh.Builtins.Args ( withArgs, flagOn )
+import System.Console.ShSh.Builtins.Args ( withArgs, flagOn, flag )
 
 import Control.Monad.Trans ( liftIO )
 import System.Exit ( ExitCode(..) )
 import Data.List ( sort )
 import System.Directory ( doesFileExist, getDirectoryContents )
+#ifdef UNIX
+import System.Posix.Files ( getSymbolicLinkStatus, isDirectory, linkCount,
+                            fileSize,
+                            ownerReadMode, ownerWriteMode, ownerExecuteMode,
+                            groupReadMode, groupWriteMode, groupExecuteMode,
+                            otherReadMode, otherWriteMode, otherExecuteMode,
+                            fileMode, nullFileMode, intersectFileModes )
+#endif
 
 {-# NOINLINE ls #-}
 ls :: [String] -> Shell ExitCode
 ls = withArgs "ls" header args ls'
     where ls' [] = do fs <- liftIO (getDirectoryContents ".")
-                      oPutStr $ unlines $ sort $
+                      amlong <- flag 'l'
+                      let lsone = if amlong then lsfilelong else oPutStrLn
+                      mapM_ lsone $ sort $
                               filter unboring fs
                       return ExitSuccess
           ls' [f] = do isf <- liftIO $ doesFileExist f
+                       amlong <- flag 'l'
+                       let lsone = if amlong then lsfilelong else oPutStrLn
                        if isf
-                        then oPutStrLn f
+                        then lsone f
                         else do cs <- liftIO $ getDirectoryContents f
-                                oPutStr $ unlines $ drop 2 $ sort cs
+                                mapM_ lsone $ drop 2 $ sort cs
                        return ExitSuccess
           ls' fs =
               do x <- mapM (liftIO . lsf) fs
-                 oPutStr $ unlines x
+                 amlong <- flag 'l'
+                 let lsone = if amlong then lsfilelong else oPutStrLn
+                 mapM_ lsone x
                  return ExitSuccess
-          header = "Usage: ls [file]..."
-          args = [flagOn "l" [] 'l' "this is ignored"]
+          header = "Usage: ls [option].. [file]..."
+          args = [flagOn "l" [] 'l' "use a long listing format"]
           unboring ('.':_) = False
           unboring _ = True
           lsf f = do isf <- doesFileExist f
@@ -37,3 +51,28 @@ ls = withArgs "ls" header args ls'
                         else do cs <- sort `fmap` getDirectoryContents f
                                 return $ unlines ((f++":") :
                                                     map ("  "++) (drop 2 cs))
+#ifdef UNIX
+          lsfilelong f = do stat <- liftIO $ getSymbolicLinkStatus f
+                            oPutStr $ if isDirectory stat then "d" else "-"
+                            oPutStr (showMode $ fileMode stat)
+                            oPutStr $ show (linkCount stat)++" "
+                            oPutStr $ pad 8 (show $ fileSize stat)++" "
+                            oPutStrLn f
+                            return ()
+          showMode m = showOne 'r' ownerReadMode m:
+                       showOne 'w' ownerWriteMode m:
+                       showOne 'x' ownerExecuteMode m:
+                       showOne 'r' groupReadMode m:
+                       showOne 'w' groupWriteMode m:
+                       showOne 'x' groupExecuteMode m:
+                       showOne 'r' otherReadMode m:
+                       showOne 'w' otherWriteMode m:
+                       showOne 'x' otherExecuteMode m:
+                                   " "
+          showOne c x m = if intersectFileModes x m == nullFileMode
+                          then '-'
+                          else c
+          pad n s = if length s >= n then s else (take (n-length s) $ repeat ' ')++s
+#else
+          lsfilelong f = oPutStrLn f
+#endif
